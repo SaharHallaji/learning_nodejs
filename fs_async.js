a5a2edece8331fb29bf8db405c59bf3d069901c2
sahar <saharhallaji.dev@gmail.com>
2023-08-23 00:45:57 +0330

    practice

--- fs_async.js
+++ fs_async.js
@@ -1,4 +1,6 @@
 const {readFile, writeFile} = require("fs");
+const path = require("path");
+/*
 console.log("start")
 readFile('./content/first.txt',"utf-8", (err, result) => {
     let first = result ;
@@ -14,8 +16,22 @@ readFile('./content/first.txt',"utf-8", (err, result) => {
 console.log("starting the next task!")
 
 
+*/
 
+const getText = (path) =>{
+    return new Promise((resolve , reject)=>{
+        readFile(path , 'utf8' , (err , data)=>{
+            if (err) {
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
+}
 
-
+getText('./content/first.txt')
+    .then((result)=> console.log(result))
+    .catch((err)=> console.log(err))
 
 
